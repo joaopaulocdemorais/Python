5c3d01bf6ccc065c0606f94d7ee34465f22c78f3
Joao Paulo <joaopaulomorais2@gmail.com>
2023-01-09 20:19:17 -0300

    09/01/2023

---
+++ Curso de Pytho/ex006.py
@@ -0,0 +1,9 @@
+n = int(input('Informe um numero: '))
+
+d = n + n
+t = n * 3
+r = n ** (1/2)
+
+print('O dobor de {} vale {}'.format(n, d))
+print('O triplo de {} vale {}.'.format(n, t))
+print('O raiz quadrada de {} é igual a {:.3f}'.format(n, r))
